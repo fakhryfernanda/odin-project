cbe80b5b04ea853575577db1268b45ce7eef7456
fakhryfernanda <mff023@gmail.com>
2022-05-15 12:38:52 +0700

    Add 2 module examples

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,10 +1,15 @@
 import _ from 'lodash';
+import myName from './myName';
+import {functionOne, functionTwo} from './myModule';
 
 function component() {
     const element = document.createElement('div');
   
     // lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    // use your own function
+    element.textContent = myName('Cody');
   
     return element;
 }
